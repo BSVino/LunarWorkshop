3a645e589562c0f6893ef984516738b395c196a2
Jorge Rodriguez <jorge@lunarworkshop.net>
2010-04-22 00:12:32 +0000

    Rebranding

---
+++ utils/license/license.h
@@ -0,0 +1,11 @@
+#ifndef CF_LICENSE_H
+#define CF_LICENSE_H
+#ifdef _WIN32
+#pragma once
+#endif
+
+#include <string>
+
+extern bool GenerateKey(std::string sCode, std::string& sKey);
+
+#endif
---
+++ utils/modelutility/shaders/shaders.h
@@ -0,0 +1,13 @@
+#ifndef MODELUTIL_SHADER_H
+#define MODELUTIL_SHADER_H
+
+extern const char* GetVSModelShader();
+extern const char* GetFSModelShader();
+
+extern const char* GetVSFlattenedShadowMap();
+extern const char* GetFSFlattenedShadowMap();
+
+extern const char* GetVSAOMap();
+extern const char* GetFSAOMap();
+
+#endif
