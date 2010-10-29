ef6af1bc4a88f95cdff32f552e1d4ba7e44e1a7e
Jorge Rodriguez <jorge@lunarworkshop.net>
2010-10-29 22:36:30 +0000

    Plus license management and demo limitations.

--- license.cpp
+++ license.cpp
@@ -4,49 +4,7 @@
 
 #include <strutils.h>
 #include <platform.h>
-
-#define MT_SIZE 624
-static size_t g_aiMT[MT_SIZE];
-static size_t g_iMTI = 0;
-
-// Mersenne Twister implementation from Wikipedia.
-void mtsrand(size_t iSeed)
-{
-	g_aiMT[0] = iSeed;
-	for (size_t i = 1; i < MT_SIZE; i++)
-	{
-		size_t inner1 = g_aiMT[i-1];
-		size_t inner2 = (g_aiMT[i-1]>>30);
-		size_t inner = inner1 ^ inner2;
-		g_aiMT[i] = (0x6c078965 * inner) + i;
-	}
-
-	g_iMTI = 0;
-}
-
-size_t mtrand()
-{
-	if (g_iMTI == 0)
-	{
-		for (size_t i = 0; i < MT_SIZE; i++)
-		{
-			size_t y = (0x80000000&(g_aiMT[i])) + (0x7fffffff&(g_aiMT[(i+1) % MT_SIZE]));
-			g_aiMT[i] = g_aiMT[(i + 397)%MT_SIZE] ^ (y>>1);
-			if ((y%2) == 1)
-				g_aiMT[i] = g_aiMT[i] ^ 0x9908b0df;
-		}
-	}
-
-	size_t y = g_aiMT[g_iMTI];
-	y = y ^ (y >> 11);
-	y = y ^ ((y << 7) & (0x9d2c5680));
-	y = y ^ ((y << 15) & (0xefc60000));
-	y = y ^ (y >> 18);
-
-	g_iMTI = (g_iMTI + 1) % MT_SIZE;
-
-	return y;
-}
+#include <mtrand.h>
 
 bool GenerateKey(std::string sProductCode, std::string& sKey)
 {
@@ -73,9 +31,6 @@ bool GenerateKey(std::string sProductCode, std::string& sKey)
 		aiHostID[i] = (unsigned char)iID;
 	}
 
-	if (!iProductCode)
-		return false;
-
 	unsigned char szTexture[40] =
 	{
 		0xb3, 0x5c, 0x5a, 0xdd, 0x83, 0xdf, 0xba, 0xd3, 0xf6, 0x99,
@@ -86,34 +41,10 @@ bool GenerateKey(std::string sProductCode, std::string& sKey)
 
 	size_t i;
 
-#ifdef CF_LICENSE_GENERATOR
-	bool bFoundId = true;
-#else
-	unsigned char* paiAddresses;
-	size_t iAddresses;
-	GetMACAddresses(paiAddresses, iAddresses);
-
-	bool bFoundId = false;
-	for (i = 0; i < iAddresses; i++)
-	{
-		if (memcmp(&aiHostID[0], &paiAddresses[i*8], sizeof(aiHostID)) == 0)
-		{
-			bFoundId = true;
-			break;
-		}
-	}
-#endif
-
 	unsigned int iIdSum = 0;
 
-	// Anybody with no ethernet card falls back to just the product code.
-	// Technically it means all of those people can share a code/key but
-	// everybody has nic cards these days so who cares?
-	if (bFoundId)
-	{
-		for (i = 0; i < 8; i++)
-			iIdSum += aiHostID[i];
-	}
+	for (i = 0; i < 8; i++)
+		iIdSum += aiHostID[i];
 
 	mtsrand(iProductCode + iIdSum);
 
