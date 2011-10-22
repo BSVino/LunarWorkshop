530fc5fece12b1440a9a398ade02de7d78927336
Jorge Rodriguez <jorge@lunarworkshop.com>
2011-10-22 01:10:19 -0700

    Show me Tack's health!

--- tack/src/ui/hud.cpp
+++ tack/src/ui/hud.cpp
@@ -4,6 +4,7 @@
 #include <tengine/renderer/renderer.h>
 
 #include "../characters/tack_character.h"
+#include "../tack_game.h"
 
 CTackHUD::CTackHUD()
 	: glgui::CPanel(0, 0, glgui::CRootPanel::Get()->GetWidth(), glgui::CRootPanel::Get()->GetHeight())
@@ -12,4 +13,12 @@ CTackHUD::CTackHUD()
 
 void CTackHUD::Paint(int x, int y, int w, int h)
 {
+	glgui::CBaseControl::PaintRect(95, 15, 50, 55, Color(50, 50, 50, 150));
+
+	glgui::CLabel::PaintText("Tack", 4, "sans-serif", 18, 100, 40);
+
+	CTackCharacter* pTack = TackGame()->GetLocalPlayerCharacter();
+	tstring sHealth = sprintf("%d%%", (int)(pTack->GetHealth()*100/pTack->GetTotalHealth()));
+
+	glgui::CLabel::PaintText(sHealth, sHealth.length(), "sans-serif", 18, 100, 60);
 }
