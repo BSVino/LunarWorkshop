c1feaaeb23461358ba62212289e72140b57e4eba
Jorge Rodriguez <jorge@lunarworkshop.com>
2011-11-07 14:16:45 -0800

    Fix misaligned text since new label code.

--- tack/src/ui/hud.cpp
+++ tack/src/ui/hud.cpp
@@ -22,10 +22,10 @@ void CTackHUD::Paint(float x, float y, float w, float h)
 	CPlayerCharacter* pTack = dynamic_cast<CPlayerCharacter*>(TackGame()->GetLocalPlayerCharacter());
 
 	tstring sHealth = sprintf("Tack - %d%%", (int)(pTack->GetHealth()*100/pTack->GetTotalHealth()));
-	glgui::CLabel::PaintText(sHealth, sHealth.length(), "sans-serif", 18, 100, 35);
+	glgui::CLabel::PaintText(sHealth, sHealth.length(), "sans-serif", 18, 100, 20);
 
 	tstring sAbility = tstring("Ability: ") + SpecialAbilityName(pTack->GetSpecialAbility());
-	glgui::CLabel::PaintText(sAbility, sAbility.length(), "sans-serif", 18, 100, 60);
+	glgui::CLabel::PaintText(sAbility, sAbility.length(), "sans-serif", 18, 100, 45);
 
 	float flCorpseAbsorbDistance = pTack->CorpseAbsorbDistance();
 	CCorpse* pNearestCorpse = NULL;
@@ -56,7 +56,7 @@ void CTackHUD::Paint(float x, float y, float w, float h)
 		tstring sTip = "F - Absorb '" + SpecialAbilityName(pNearestCorpse->GetSpecialAbility()) + "'";
 		float flTextWidth = glgui::CLabel::GetTextWidth(sTip, sTip.length(), "sans-serif", 18);
 		float flFontHeight = glgui::CLabel::GetFontHeight("sans-serif", 18);
-		glgui::CBaseControl::PaintRect(w/2+200 - 5, h/2 - flFontHeight, flTextWidth + 10, flFontHeight + 10, Color(50, 50, 50, 150));
-		glgui::CLabel::PaintText(sTip, sTip.length(), "sans-serif", 18, (float)w/2+200, (float)h/2);
+		glgui::CBaseControl::PaintRect(w/2+200 - 5, h/2 - flFontHeight - 5, flTextWidth + 10, flFontHeight + 10, Color(50, 50, 50, 150));
+		glgui::CLabel::PaintText(sTip, sTip.length(), "sans-serif", 18, (float)w/2+200, (float)h/2-flFontHeight);
 	}
 }
