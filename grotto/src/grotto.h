6c4aa79b1b23098ccdbb4f52df88f89f2c3b067d
Jorge Rodriguez <jorge@lunarworkshop.com>
2013-05-11 11:50:04 -0700

    Remove grotto because it's the 2D version that we don't want anymore.

--- grotto/src/grotto.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-typedef enum
-{
-	REFLECTION_ANY = ~0,
-	REFLECTION_NONE = 0,
-	REFLECTION_LATERAL,		// Left/right is reflected, due to a vertically placed mirror
-	REFLECTION_VERTICAL,	// Up/down is reflected, due to a horizontally placed mirror
-} reflection_t;
