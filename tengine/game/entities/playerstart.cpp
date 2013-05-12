74eadcce4dceb118a2c4019a25f8a2981599aeee
Jorge Rodriguez <jorge@lunarworkshop.com>
2013-05-12 10:03:49 -0700

    Playerstart z up.

--- tengine/game/entities/playerstart.cpp
+++ tengine/game/entities/playerstart.cpp
@@ -6,7 +6,7 @@ NETVAR_TABLE_BEGIN(CPlayerStart);
 NETVAR_TABLE_END();
 
 SAVEDATA_TABLE_BEGIN_EDITOR(CPlayerStart);
-	SAVEDATA_OVERRIDE_DEFAULT(CSaveData::DATA_COPYTYPE, AABB, m_aabbBoundingBox, "BoundingBox", AABB(Vector(-0.35f, 0, -0.35f), Vector(0.35f, 2, 0.35f)));
+	SAVEDATA_OVERRIDE_DEFAULT(CSaveData::DATA_COPYTYPE, AABB, m_aabbBoundingBox, "BoundingBox", AABB(Vector(-0.35f, -0.35f, 0), Vector(0.35f, 0.35f, 2)));
 SAVEDATA_TABLE_END();
 
 INPUTS_TABLE_BEGIN(CPlayerStart);
